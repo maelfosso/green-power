7c979e021de71dc6042793c3ba2d7150449933be
Mael FOSSO <fosso.mael.elvis@gmail.com>
2020-03-14 14:33:27 -0400

    Set a basic structure

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,10 @@
 import React from 'react';
-import './App.css';
+import '../App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        
         <a
           className="App-link"
           href="https://reactjs.org"
