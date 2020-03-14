e5bf6eed93cf83a5513194bb23a007579033821f
Mael FOSSO <fosso.mael.elvis@gmail.com>
2020-03-14 19:19:51 -0400

    Header is OK

--- src/components/App.js
+++ src/components/App.js
@@ -1,19 +1,26 @@
 import React from 'react';
-import '../App.css';
+import '../App.scss';
+import DataEntry from './DataEntry';
+import Result from './Result';
+import Details from './Details';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div>
+      <header>
+        <span className="logo-wrapper">
+          <i className="logo" />
+          <span>Green Power</span>
+        </span>
+        <div className="button">
+        </div>
       </header>
+      
+      <div className="App">
+        <DataEntry />
+        <Result />
+        <Details />
+      </div>
     </div>
   );
 }
---
+++ src/components/DataEntry.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function DataEntry() {
+  return (
+    <div className="Data-Entry">
+      <h1>Data entry</h1>
+    </div>
+  );
+}
+
+export default DataEntry;
