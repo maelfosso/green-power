ef550caf846ffc9796c10408d81f57db2e2cb733
Mael FOSSO <fosso.mael.elvis@gmail.com>
2020-03-14 21:02:14 -0400

    Result ui is OK

--- src/components/Result.js
+++ src/components/Result.js
@@ -1,9 +1,33 @@
 import React from 'react';
+import { Statistic, Row, Col, Button, PageHeader } from 'antd';
+
 
 function Result() {
   return (
     <div className="Result">
-      <h1>Result</h1>
+      <PageHeader
+        title="Result"
+      >
+        <Row gutter={[48, 16]} justify="space-between">
+          <Col>
+            <Statistic title="Prix du Kwh hors pointe (FCFA)" value={60} />
+          </Col>
+          <Col>
+            <Statistic title="Prix du Kwh pointe (FCFA)" value={85} />
+          </Col>
+          <Col>
+            <Statistic title="Facture HT (FCFA)" value={4661505} />
+          </Col>
+          <Col>
+            <Statistic title="TVA" value={897340} />
+          </Col>
+          <Col>
+            <Statistic title="Facture TTC (FCFA)" value={5558845} />
+          </Col>
+        </Row>
+      
+      </PageHeader>
+
     </div>
   );
 }
