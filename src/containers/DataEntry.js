da3d476ff1da135d1e06812e17be3c1ef75dc268
Mael FOSSO <fosso.mael.elvis@gmail.com>
2020-03-17 04:23:27 -0400

    Contrat d'abonnement section arrange

--- src/containers/DataEntry.js
+++ src/containers/DataEntry.js
@@ -18,6 +18,24 @@ const DataEntry = props => {
   const [contratForm] = Form.useForm();
   const [factureForm] = Form.useForm();
 
+  const layout = {
+    labelCol: {
+      xs: { span: 12 },
+      sm: { span: 16 },
+      md: { span: 16 }
+    },
+    wrapperCol: {
+      xs: { span: 12 },
+      sm: { span: 8 }
+    },
+  };
+  const tailLayout = {
+    wrapperCol: {
+      offset: 8,
+      span: 16,
+    },
+  };
+
   const onCalculate = async () => {
     const { simulate } = props;
 
@@ -29,50 +47,53 @@ const DataEntry = props => {
 
   return (
     <div className="Data-Entry">
+      <Card title="CONTRAT D'ABONNEMENT">
+        <Form
+          { ...layout }
+          labelAlign="left"
+          name="contrat"
+          form={contratForm}
+          layout="horizontal"
+          initialValues={{
+            size: componentSize,
+          }}
+          size={componentSize}
+        >
+          <Form.Item
+            label="Puissance souscrite (KW)"
+            name="pscrite"
+          >
+            <InputNumber formatter={value => `${value} KW`} parser={value => value.replace(' KW', '')}/>
+          </Form.Item>
+          <Form.Item
+            label="Puissance transformateur (KVA)"
+            name="ptrans"
+          >
+            <InputNumber formatter={value => `${value} KVA`} parser={value => value.replace(' KVA', '')}/>
+          </Form.Item>
+          <Form.Item
+            label="Puissance facturee (KVA)"
+            name="ptfact"
+          >
+            <InputNumber formatter={value => `${value} KVA`} parser={value => value.replace(' KVA', '')}/>
+          </Form.Item>
+          <Form.Item label="Type contrat" name="typecontrat">
+            <Radio.Group>
+              <Radio.Button value="mt_bt">MT/BT</Radio.Button>
+              <Radio.Button value="mt_mt">MT/MT</Radio.Button>
+            </Radio.Group>
+          </Form.Item>
+          <Form.Item label="Location transfo" name="loctrans">
+            <Radio.Group>
+              <Radio value="oui">Oui</Radio>
+              <Radio value="non">Non</Radio>
+            </Radio.Group>
+          </Form.Item>
+        </Form>
+      </Card>
       <Row gutter={[16, 16]} justify="space-between">
         <Col flex={1} xs={32} sm={32} md={12} lg={12}>
-          <Card title="Contrat abonnement">
-            <Form
-              name="contrat"
-              form={contratForm}
-              layout="vertical"
-              initialValues={{
-                size: componentSize,
-              }}
-              size={componentSize}
-            >
-              <Form.Item
-                label="Puissance souscrite (KW)"
-                name="pscrite"
-              >
-                <InputNumber />
-              </Form.Item>
-              <Form.Item
-                label="Puissance transformateur (KVA)"
-                name="ptrans"
-              >
-                <InputNumber />
-              </Form.Item>
-              <Form.Item
-                label="Puissance transformateur facturee (KVA)"
-                name="ptfact"
-              >
-                <InputNumber />
-              </Form.Item>
-              <Form.Item label="Type contrat" name="typecontrat">
-                <Radio.Group>
-                  <Radio.Button value="mt_bt">MT/BT</Radio.Button>
-                  <Radio.Button value="mt_mt">MT/MT</Radio.Button>
-                </Radio.Group>
-              </Form.Item>
-              <Form.Item label="Location transformation" name="loctrans">
-                <Radio.Group>
-                  <Radio value="oui">Oui</Radio>
-                  <Radio value="non">Non</Radio>
-                </Radio.Group>
-              </Form.Item>
-            </Form>
-          </Card>
+          
         </Col>
         <Col flex={1} xs={32} sm={32} md={12} lg={12}>
           <Card title="Facture">
